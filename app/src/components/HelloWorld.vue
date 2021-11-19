a6bd1d11d00aa3fa9810c3fd2ad8b3784e651c21
Eri Sunarko <erisunarko@travikr.com>
2021-11-19 18:38:35 +0700

    Simple fetch data from API

---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="input-container">
+      <input type="text" placeholder="First Name" v-model="firstName" />
+    </div>
+    <div class="input-container">
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+    </div>
+    <div class="input-container">
+      <span> Hi, {{ fullname }} </span>
+    </div>
+    <my-button btnText="Save" color="blue" @submit="onClickBtn" />
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+import { mapState } from "vuex";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    "my-button": Button,
+  },
+  data: () => ({
+    firstName: "",
+    lastName: "",
+  }),
+  computed: {
+    ...mapState({
+      fullname: (state) => state.helloworld.fullName,
+    }),
+  },
+  methods: {
+    onClickBtn() {
+      console.log("Submit triggered from child component!");
+      console.log("First Name: " + this.firstName);
+      console.log("Last Name: " + this.lastName);
+      this.$store.dispatch(
+        "helloworld/actSetFullName",
+        this.firstName.concat(" ").concat(this.lastName)
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-container {
+  padding: 6px;
+}
+</style>
